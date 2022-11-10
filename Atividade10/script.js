2832772e41259ce9691b81e4a95c0238ae99a1fb
Gabriel Claudino Leão <78832392+Gabrielclf10@users.noreply.github.com>
2022-11-09 21:53:06 -0300

    Add files via upload

---
+++ Atividade10/script.js
@@ -0,0 +1,18 @@
+var aluno1 = new Object();
+aluno1.name = "Gabriel";
+aluno1.RA = "123";
+
+alert("\nAluno 1 \n Nome: " + aluno1.name + "\n RA: " + aluno1.RA);
+
+var aluno2 = {
+    name: "Carlo",
+    RA: "456"
+};
+
+alert("\nAluno 2 \n Nome: " + aluno2.name + "\n RA: " + aluno2.RA);
+
+var aluno3 = {};
+aluno3.name = "Tunga";
+aluno3.RA = "789";
+
+alert("\nAluno 3 \n Nome: " + aluno3.name + "\n RA: " + aluno3.RA);
